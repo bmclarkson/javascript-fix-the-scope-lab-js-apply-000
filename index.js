9617059a83c958c9bf2dbfe998693560554e6782
Brass Clarkson <bm.clarkson@gmail.com>
2018-04-11 01:43:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function myAnimal() {
   var animal = 'dog';
   return animal
 }
+
+function yourAnimal() {
+  var animal = 'cat';
+  return animal;
+}
+function add2(n) {
+  var two = 2
+  return n + two;
+}
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
